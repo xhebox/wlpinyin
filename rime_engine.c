5ed30789568c3572673073f5aab47f9da17a5c65
xhe <xw897002528@gmail.com>
2025-12-26 11:19:45 +0800

    remove unused memory clear

--- rime_engine.c
+++ rime_engine.c
@@ -29,15 +29,12 @@ static void im_engine_update(rime_engine *engine) {
 	RimeApi *api = engine->api;
 
 	api->free_status(&engine->status);
-	RIME_STRUCT_CLEAR(engine->status);
 	api->get_status(engine->sess, &engine->status);
 
 	api->free_context(&engine->context);
-	RIME_STRUCT_CLEAR(engine->context);
 	api->get_context(engine->sess, &engine->context);
 
 	api->free_commit(&engine->commit);
-	RIME_STRUCT_CLEAR(engine->commit);
 	api->get_commit(engine->sess, &engine->commit);
 }
 
